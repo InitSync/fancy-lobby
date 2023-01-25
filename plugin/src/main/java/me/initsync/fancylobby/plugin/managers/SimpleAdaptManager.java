e553609da83b61d24bff2d18c63ba6fb2aa7d97b
InitSync <benjaalejandro206@gmail.com>
2023-01-25 20:43:49 -0300

    feat(SimpleAdaptManager): add implementation of AdaptManager for manage and get the server versions adapts

---
+++ plugin/src/main/java/me/initsync/fancylobby/plugin/managers/SimpleAdaptManager.java
@@ -0,0 +1,59 @@
+package me.initsync.fancylobby.plugin.managers;
+
+import com.google.common.base.Preconditions;
+import me.initsync.fancylobby.api.model.adapt.AdaptManager;
+import me.initsync.fancylobby.api.model.adapt.ServerAdapt;
+import me.initsync.fancylobby.plugin.utils.LogUtils;
+import org.bukkit.Bukkit;
+
+public class SimpleAdaptManager implements AdaptManager {
+	private ServerAdapt adapt;
+	
+	public SimpleAdaptManager() {}
+	
+	@Override
+	public AdaptManager of(ServerAdapt adapt) {
+		Preconditions.checkNotNull(adapt, "The server adapt specified is null.");
+		
+		if (!requiresAdapt()) return null;
+		
+		this.adapt = adapt;
+		return null;
+	}
+	
+	@Override
+	public AdaptManager find() {
+		if (!requiresAdapt()) return null;
+		
+		String packageName = Bukkit.getServer().getClass().getPackage().getName();
+		String version = packageName.substring(packageName.lastIndexOf('.') + 1);
+		
+		try {
+			final Class<?> clazz = Class.forName("me.initsync.fancylobby.adapt." + version + ".AdaptHandler");
+			if (ServerAdapt.class.isAssignableFrom(clazz)) adapt = (ServerAdapt) clazz.getConstructor().newInstance();
+		} catch (Exception exception) {
+			LogUtils.error("Has happens an error to install the adapt for the server version.");
+			LogUtils.error("The plugin requires the latest version of the release, example: 1.8 -> 1.8.8 (1_8_R3)");
+			LogUtils.error("Or you will can use a version lower than 1.8. The plugin doesn't support versions 1.7.10 or older!");
+			exception.printStackTrace();
+			return null;
+		}
+		
+		LogUtils.info("Loading adapt for Minecraft " + version);
+		
+		return this;
+	}
+	
+	@Override
+	public ServerAdapt getAdapt() {
+		if (adapt == null) throw new IllegalStateException("Cannot get the server adapt because isn't installed, or your server version is +1.16!");
+		
+		return adapt;
+	}
+	
+	private boolean requiresAdapt() {
+		return Integer.parseInt(Bukkit.getBukkitVersion()
+			 .split("-")[0]
+			 .split("\\.")[1]) < 17;
+	}
+}
