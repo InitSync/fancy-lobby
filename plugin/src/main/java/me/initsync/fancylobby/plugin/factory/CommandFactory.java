57d36887024b53fff08b19ba2835221775f3fdf4
InitSync <benjaalejandro206@gmail.com>
2023-01-26 00:12:10 -0300

    feat(CommandFactory): add factory for load commands rapidly

---
+++ plugin/src/main/java/me/initsync/fancylobby/plugin/factory/CommandFactory.java
@@ -0,0 +1,54 @@
+package me.initsync.fancylobby.plugin.factory;
+
+import com.google.common.base.Preconditions;
+import me.initsync.fancylobby.plugin.FancyLobby;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
+
+import java.util.Objects;
+
+public interface CommandFactory {
+	static Builder newLoader(FancyLobby plugin) {
+		return new Builder(plugin);
+	}
+	
+	class Builder {
+		private final FancyLobby plugin;
+		
+		private String command;
+		private CommandExecutor executor;
+		private TabCompleter completer;
+		
+		public Builder(FancyLobby plugin) {
+			this.plugin = Objects.requireNonNull(plugin, "The FancyLobby instance is null.");
+		}
+		
+		public Builder command(String command) {
+			this.command = Objects.requireNonNull(command, "The command is null.");
+			Preconditions.checkArgument(!command.isEmpty(), "The command is empty.");
+			return this;
+		}
+		
+		public Builder executor(CommandExecutor executor) {
+			this.executor = Objects.requireNonNull(executor, "The command executor is null.");
+			return this;
+		}
+		
+		public Builder completer(TabCompleter completer) {
+			this.completer = Objects.requireNonNull(completer, "The tab completer is null.");
+			return this;
+		}
+		
+		public void register() {
+			if (command == null || executor == null) return;
+			
+			final PluginCommand pluginCommand = plugin.getCommand(command);
+			if (pluginCommand == null) return;
+			
+			pluginCommand.setExecutor(executor);
+			
+			if (completer != null) pluginCommand.setTabCompleter(completer);
+		}
+	}
+}
