bdfa142a42822b2782bda448189e614d41c922eb
InitSync <benjaalejandro206@gmail.com>
2023-01-26 00:16:54 -0300

    chore(PlayerJoinListener): implement methods from `Utils` class for tests

--- plugin/src/main/java/me/initsync/fancylobby/plugin/listener/PlayerJoinListener.java
+++ plugin/src/main/java/me/initsync/fancylobby/plugin/listener/PlayerJoinListener.java
@@ -2,11 +2,13 @@ package me.initsync.fancylobby.plugin.listener;
 
 import me.initsync.fancylobby.api.model.manager.LanguageManager;
 import me.initsync.fancylobby.api.model.manager.ScoreboardManager;
+import me.initsync.fancylobby.plugin.utils.Utils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.Objects;
 
@@ -25,5 +27,13 @@ public class PlayerJoinListener implements Listener {
 		
 		languageManager.applyDefaultLanguage(player.getUniqueId());
 		scoreboardManager.create(player);
+		
+		Utils.setTabList(player, "<br>&cTabList Header!<br>", "<br>&elocalhost");
+		Utils.setPlayerTag(player, "&c[Admin] ", "&a [null]");
+	}
+	
+	@EventHandler (priority = EventPriority.LOWEST)
+	public void onQuit(PlayerQuitEvent event) {
+		scoreboardManager.remove(event.getPlayer().getUniqueId());
 	}
 }
