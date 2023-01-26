f7ffbec786ae9518ae03bf3d1faa6f7c69957fdf
InitSync <benjaalejandro206@gmail.com>
2023-01-25 21:36:20 -0300

    feat(CommandRegisterEvent): add custom event for when the player register a custom command

---
+++ api/src/main/java/me/initsync/fancylobby/api/event/commands/CommandRegisterEvent.java
@@ -0,0 +1,49 @@
+package me.initsync.fancylobby.api.event.commands;
+
+import com.google.common.base.Preconditions;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import java.util.Objects;
+
+public class CommandRegisterEvent extends Event implements Cancellable {
+	private static final HandlerList HANDLER_LIST = new HandlerList();
+	
+	private final String command;
+	
+	private boolean cancelled;
+	
+	public CommandRegisterEvent(String command) {
+		this.command = Objects.requireNonNull(command, "The command is null.");
+		Preconditions.checkArgument(!command.isEmpty(), "The command is empty.");
+	}
+	
+	/**
+	 * Returns the command registered.
+	 *
+	 * @return The command registered.
+	 */
+	public String getCommand() {
+		return command;
+	}
+	
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+	
+	@Override
+	public void setCancelled(boolean cancel) {
+		cancelled = cancel;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return HANDLER_LIST;
+	}
+	
+	public static HandlerList getHandlerList() {
+		return HANDLER_LIST;
+	}
+}
