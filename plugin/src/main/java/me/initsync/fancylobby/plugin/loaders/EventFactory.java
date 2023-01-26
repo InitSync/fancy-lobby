800b073e898f7acb58bd776a6008fe044a1ce931
InitSync <benjaalejandro206@gmail.com>
2023-01-25 21:27:56 -0300

    feat(EventFactory): add interface/class for load listeners

---
+++ plugin/src/main/java/me/initsync/fancylobby/plugin/loaders/EventFactory.java
@@ -0,0 +1,35 @@
+package me.initsync.fancylobby.plugin.loaders;
+
+import me.initsync.fancylobby.plugin.FancyLobby;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+
+import java.util.Objects;
+
+public interface EventFactory {
+	static Loader newLoader(FancyLobby plugin) {
+		return new Loader(plugin);
+	}
+	
+	class Loader {
+		private final FancyLobby plugin;
+		
+		private Listener[] listeners;
+		
+		public Loader(FancyLobby plugin) {
+			this.plugin = Objects.requireNonNull(plugin, "The FancyLobby instance is null.");
+		}
+		
+		public Loader providers(Listener... listeners) {
+			this.listeners = Objects.requireNonNull(listeners, "The listeners array is null.");
+			return this;
+		}
+		
+		public void load() {
+			if (listeners == null) return;
+			
+			final PluginManager manager = plugin.getServer().getPluginManager();
+			for (Listener listener : listeners) manager.registerEvents(listener, plugin);
+		}
+	}
+}
