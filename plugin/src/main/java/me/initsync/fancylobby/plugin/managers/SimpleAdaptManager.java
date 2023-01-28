ea285b3d7b35a974b4146a24af8638a3ad3656f2
InitSync <benjaalejandro206@gmail.com>
2023-01-28 01:13:58 -0300

    chore(SimpleAdaptManager): remove `requiresAdapt()` method and import as static the `error()` method of `LogUtils`

--- plugin/src/main/java/me/initsync/fancylobby/plugin/managers/SimpleAdaptManager.java
+++ plugin/src/main/java/me/initsync/fancylobby/plugin/managers/SimpleAdaptManager.java
@@ -6,6 +6,8 @@ import me.initsync.fancylobby.api.model.adapt.ServerAdapt;
 import me.initsync.fancylobby.plugin.utils.LogUtils;
 import org.bukkit.Bukkit;
 
+import static me.initsync.fancylobby.plugin.utils.LogUtils.error;
+
 public class SimpleAdaptManager implements AdaptManager {
 	private ServerAdapt adapt;
 	
@@ -15,7 +17,11 @@ public class SimpleAdaptManager implements AdaptManager {
 	public AdaptManager of(ServerAdapt adapt) {
 		Preconditions.checkNotNull(adapt, "The server adapt specified is null.");
 		
-		if (!requiresAdapt()) return null;
+		if (Integer.parseInt(Bukkit.getBukkitVersion()
+			 .split("-")[0]
+			 .split("\\.")[1]) < 17) {
+			return null;
+		}
 		
 		this.adapt = adapt;
 		return null;
@@ -23,7 +29,11 @@ public class SimpleAdaptManager implements AdaptManager {
 	
 	@Override
 	public AdaptManager find() {
-		if (!requiresAdapt()) return null;
+		if (Integer.parseInt(Bukkit.getBukkitVersion()
+			 .split("-")[0]
+			 .split("\\.")[1]) < 17) {
+			return null;
+		}
 		
 		String packageName = Bukkit.getServer().getClass().getPackage().getName();
 		String version = packageName.substring(packageName.lastIndexOf('.') + 1);
@@ -32,9 +42,9 @@ public class SimpleAdaptManager implements AdaptManager {
 			final Class<?> clazz = Class.forName("me.initsync.fancylobby.adapt." + version + ".AdaptHandler");
 			if (ServerAdapt.class.isAssignableFrom(clazz)) adapt = (ServerAdapt) clazz.getConstructor().newInstance();
 		} catch (Exception exception) {
-			LogUtils.error("Has happens an error to install the adapt for the server version.");
-			LogUtils.error("The plugin requires the latest version of the release, example: 1.8 -> 1.8.8 (1_8_R3)");
-			LogUtils.error("Or you will can use a version lower than 1.8. The plugin doesn't support versions 1.7.10 or older!");
+			error("Has happens an error to install the adapt for the server version.");
+			error("The plugin requires the latest version of the release, example: 1.8 -> 1.8.8 (1_8_R3)");
+			error("Or you will can use a version lower than 1.8. The plugin doesn't support versions 1.7.10 or older!");
 			exception.printStackTrace();
 			return null;
 		}
@@ -49,11 +59,5 @@ public class SimpleAdaptManager implements AdaptManager {
 		if (adapt == null) throw new IllegalStateException("Cannot get the server adapt because isn't installed, or your server version is +1.16!");
 		
 		return adapt;
-	}
-	
-	private boolean requiresAdapt() {
-		return Integer.parseInt(Bukkit.getBukkitVersion()
-			 .split("-")[0]
-			 .split("\\.")[1]) < 17;
 	}
 }
